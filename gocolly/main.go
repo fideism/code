0446bbb787e2a33953c0e9dc42675dd31518435d
fideism <nignog@aliyun.com>
2022-07-13 18:02:16 +0800

    gocolly

--- gocolly/main.go
+++ gocolly/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/fideism/code/internal/config"
@@ -29,9 +30,9 @@ var count int
 
 func main() {
 	var err error
-	path := releaseDir()
-	fmt.Println(fmt.Sprintf(`%s/%s.md`, path, time.Now().Format(`2006-01-02`)))
-	f, err = os.OpenFile(fmt.Sprintf(`%s/%s.md`, path, time.Now().Format(`2006-01-02`)), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	path := config.Setting.Gocolly.RepositoryPath
+	fmt.Println(fmt.Sprintf(`%s/%s.md`, path, time.Now().Format(`2006-01`)))
+	f, err = os.OpenFile(fmt.Sprintf(`%s/%s.md`, path, time.Now().Format(`2006-01`)), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		fmt.Println(`处理日志文件失败`, err.Error())
 	}
@@ -48,10 +49,15 @@ func main() {
 	c.OnResponse(func(r *colly.Response) {
 		fmt.Println("Visited", r.Request.URL)
 	})
-	c.OnHTML("a[class='title project-namespace-path']", func(element *colly.HTMLElement) {
+	c.OnHTML("div[class='explore-trending-projects__list-item']", func(element *colly.HTMLElement) {
+		href, _ := element.DOM.Find(`a`).Attr(`href`)
+		desc := element.DOM.Find("div[class='description']").Last().Text()
+		title := element.DOM.Find("a").Text()
+
 		dealProject(project{
-			Href: element.Attr(`href`),
-			Name: element.Text,
+			Href:        strings.Trim(href, ``),
+			Name:        strings.Trim(title, ``),
+			Description: strings.Trim(desc, ``),
 		})
 	})
 
@@ -59,27 +65,28 @@ func main() {
 		fmt.Println("Finished", r.Request.URL)
 
 		// git提交
-		execCmd(fmt.Sprintf(`cd %s && git add . && git commit -m '%s' && git push origin main`, config.Setting.Gocolly.RepositoryPath, time.Now().String()))
+		//execCmd(fmt.Sprintf(`cd %s && git add . && git commit -m '%s' && git push origin master`, config.Setting.Gocolly.RepositoryPath, time.Now().String()))
 	})
 
-	// gitee 最新推荐
+	// gitee 最新推荐 热门推荐 今日热门
 	if err := c.Visit(`https://gitee.com/explore/all?order=recommend`); nil != err {
 		fmt.Println(err)
 	}
 }
 
 type project struct {
-	Name string
-	Href string
+	Name        string
+	Href        string
+	Description string
 }
 
 func releaseDir() string {
-	path := fmt.Sprintf(`%s/releases/%s`, config.Setting.Gocolly.RepositoryPath, time.Now().Format(`2006-01`))
+	path := fmt.Sprintf(`%s`, config.Setting.Gocolly.RepositoryPath)
 
 	if err := util.Mkdir(path); nil != err {
 		fmt.Println(fmt.Sprintf(`创建文件夹失败:%s`, err.Error()))
 
-		return fmt.Sprintf(`%s/releases`, config.Setting.Gocolly.RepositoryPath)
+		return fmt.Sprintf(`%s`, config.Setting.Gocolly.RepositoryPath)
 	}
 
 	return path
@@ -88,33 +95,35 @@ func releaseDir() string {
 
 func dealProject(data project) {
 	fmt.Println(`开始处理`, data)
-	count = count + 1
-	if count > 5 {
-		fmt.Println(`已处理文件数量大于5，跳过`)
-		return
-	}
-	dir := fmt.Sprintf(`%s/%s%s`, config.Setting.Gocolly.RepositoryPath, time.Now().Format(`200601`), data.Href)
+	// count = count + 1
+	// if count > 3 {
+	// 	fmt.Println(`已处理文件数量大于3，跳过`)
+	// 	return
+	// }
+	// dir := fmt.Sprintf(`%s/%s%s`, config.Setting.Gocolly.RepositoryPath, time.Now().Format(`200601`), data.Href)
 
-	if err := util.DelDir(dir); nil != err {
-		_ = fmt.Sprintf(`删除文件夹[%s]错误:%s\n`, dir, err.Error())
-		return
-	}
+	// if err := util.DelDir(dir); nil != err {
+	// 	_ = fmt.Sprintf(`删除文件夹[%s]错误:%s\n`, dir, err.Error())
+	// 	return
+	// }
 
-	if err := util.Mkdir(dir); nil != err {
-		_ = fmt.Sprintf(`创建文件夹[%s]错误:%s\n`, dir, err.Error())
-		return
-	}
+	// if err := util.Mkdir(dir); nil != err {
+	// 	_ = fmt.Sprintf(`创建文件夹[%s]错误:%s\n`, dir, err.Error())
+	// 	return
+	// }
 
-	// git clone https://gitee.com/theajack/cnchar.git /xxx/theajack/cnchar
-	if err := execCmd(fmt.Sprintf(`git clone https://gitee.com%s.git %s`, data.Href, dir)); nil != err {
-		_ = fmt.Sprintf(`执行git命令错误:%s`, err.Error())
-		return
-	}
+	// fmt.Println(fmt.Sprintf(`git clone https://gitee.com%s.git %s`, data.Href, dir))
+	// // git clone https://gitee.com/theajack/cnchar.git /xxx/theajack/cnchar
+	// if err := execCmd(fmt.Sprintf(`git clone https://gitee.com%s.git %s`, data.Href, dir)); nil != err {
+	// 	_ = fmt.Sprintf(`执行git命令错误:%s`, err.Error())
+	// 	return
+	// }
 
-	if err := util.DelDir(fmt.Sprintf(`%s/.git`, dir)); nil != err {
-		_ = fmt.Sprintf(`删除git文件目录失败:%s`, err.Error())
-		return
-	}
+	// fmt.Println(fmt.Sprintf(`%s/.git`, dir))
+	// if err := util.DelDir(fmt.Sprintf(`%s/.git`, dir)); nil != err {
+	// 	_ = fmt.Sprintf(`删除git文件目录失败:%s`, err.Error())
+	// 	return
+	// }
 
 	// 保存记录
 	writeReadme(data)
@@ -123,7 +132,7 @@ func dealProject(data project) {
 
 func writeReadme(data project) {
 
-	content := fmt.Sprintf("- [%s](https://gitee.com%s)\n", data.Name, data.Href)
+	content := fmt.Sprintf("- [%s](https://gitee.com%s)  %s \n", data.Name, data.Href, data.Description)
 
 	fmt.Println(io.WriteString(f, content))
 }
